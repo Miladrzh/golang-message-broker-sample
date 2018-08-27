2e336467d74b128dd439e61c70471080a11a0d7a
Milad <miladrzh@gmail.com>
2018-08-27 13:50:08 +0430

    Complete Ws Message Broker without sample client and server

---
+++ broker/broker.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"strings"
+)
+
+type MessageBroker struct {
+	queues map[string]MessageQueue
+}
+
+type Message struct {
+	Message string `json:"message"`
+	Time    string `json:"time"`
+}
+
+func (broker MessageBroker) listen() {
+	http.HandleFunc("/publish/", broker.handleRequest)
+	http.HandleFunc("/subscribe/", broker.handleRequest)
+	panic(http.ListenAndServe(":8000", nil))
+}
+
+func (broker MessageBroker) handleRequest(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open web socket connection", http.StatusBadRequest)
+	}
+
+	queueName := strings.Replace(r.URL.Path, "/publish/", "", 1)
+	queueName = strings.Replace(queueName, "/subscribe/", "", 1)
+	mQueue := broker.getMessageQueue(queueName)
+
+	if r.URL.Path[:9] == "/publish/" {
+		fmt.Println("sdfadfe3")
+		go mQueue.listenToPublisher(conn, queueName)
+	} else if r.URL.Path[:11] == "/subscribe/" {
+		go mQueue.listenToSubscriber(conn, queueName)
+	}
+}
+
+func (broker MessageBroker) getMessageQueue(queueName string) (MessageQueue) {
+	_, ok := broker.queues[queueName]
+	if ok == false {
+		mQueue := MessageQueue{make(map[int]chan Message)}
+		broker.queues[queueName] = mQueue
+	}
+	return broker.queues[queueName]
+}
+
+func main() {
+	broker := MessageBroker{make(map[string]MessageQueue)}
+	broker.listen()
+}
---
+++ broker/queue.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"math/rand"
+	"time"
+)
+
+// MessageQueue declaration and methods
+type MessageQueue struct {
+	channels map[int]chan Message
+}
+
+func (mQueue MessageQueue) registerSubscriber() (int) {
+	key := rand.Intn(1000000)
+	mQueue.channels[key] = make(chan Message, 10)
+	return key
+}
+
+func (mQueue MessageQueue) send(message Message) {
+	for key := range mQueue.channels {
+		fmt.Println(key)
+		mQueue.channels[key] <- message
+	}
+}
+
+func (mQueue MessageQueue) receive(key int) (chan Message) {
+	return mQueue.channels[key]
+}
+
+func (mQueue MessageQueue) listenToPublisher(conn *websocket.Conn, queueName string) {
+	for {
+		message := Message{}
+		conn.ReadJSON(&message)
+		conn.WriteJSON(message)
+		fmt.Println(message)
+		go mQueue.send(message)
+	}
+}
+
+func (mQueue MessageQueue) listenToSubscriber(conn *websocket.Conn, queueName string) {
+	subscriberChannelId := mQueue.registerSubscriber()
+	fmt.Println(subscriberChannelId)
+	for {
+		select {
+		case message := <-mQueue.receive(subscriberChannelId):
+			message.Time = time.Now().String()
+			fmt.Println(message)
+			conn.WriteJSON(message)
+		default:
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}
+	fmt.Println(subscriberChannelId)
+}
+
+// end of MessageQueue
